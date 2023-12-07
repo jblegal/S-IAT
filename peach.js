408c3ee1ab6bc0326cdf19b8451814c26acbad21
jblegal <153203881+jblegal@users.noreply.github.com>
2023-12-07 21:02:35 +0100

    Update peach.js

--- peach.js
+++ peach.js
@@ -2,63 +2,63 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 	
 	var API = new APIConstructor();
 		  return stiatExtension({
-		  category : { 
-		    name : 'Black people', //Will appear in the data.
-		    title : {
-		      media : {word : 'Black people'}, //Name of the category presented in the task.
-		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-		      height : 7 //Used to position the "Or" in the combined block.
-		    }, 
-		    media : [ //Stimuli content as PIP's media objects
-    		    	{image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
-		    ], 
-		    //Stimulus css (style)
-		    css : {color:'#31b404','font-size':'3em'}
-		  },	
-
-  		attribute1 : 
+		 category :  
 			{
-			name : 'Unpleasant', //Attribute label
-			title : {
-				media : {word : 'Negative'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli
-				{word: 'Bomb'},
-				{word: 'Abuse'},
-				{word: 'Sadness'},
-				{word: 'Pain'},
-				{word: 'Poison'},
-				{word: 'Grief'}
-			], 
-			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
-			},
-		attribute2 : 
+				name : 'Black People', //Category name to be used for feedback and logging.
+				title : {
+					media : {word : 'Black People'}, //Name of the category presented in the task.
+					css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+					height : 4 //Used to position the "Or" in the combined block.
+				}, 
+				media : [ //Stimuli
+					{word: 'Tyron'},
+					{word: 'Malik'},
+					{word: 'Terrell'},
+					{word: 'Jazmin'},
+					{word: 'Tiara'},
+					{word: 'Shanice'}
+				],
+				//Can change color and size of the targets here.
+				css : {color:'#31b404','font-size':'2em'}
+			},	
+			attribute1 : 
 			{
-			name : 'Pleasant', //Attribute label
-			title : {
-				media : {word : 'Positive'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli
-				{word: 'Paradise'},
-				{word: 'Pleasure'},
-				{word: 'Cheer'},
-				{word: 'Wonderful'},
-				{word: 'Splendid'},
-				{word: 'Love'}
-			], 
-			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
+				name : 'Unpleasant', //Attribute name to be used for feedback and logging
+				title : {
+					media : {word : 'Unpleasant'}, //Name of the category presented in the task.
+					css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+					height : 4 //Used to position the "Or" in the combined block.
+				}, 
+				media : [ //Stimuli
+					{word: 'Bomb'},
+					{word: 'Abuse'},
+					{word: 'Sadness'},
+					{word: 'Pain'},
+					{word: 'Poison'},
+					{word: 'Grief'}
+				], 
+				//Can change color and size of the targets here.
+				css : {color:'#31b404','font-size':'2em'}
 			},
+			attribute2 : 
+			{
+				name : 'Pleasant', //Attribute name to be used for feedback and logging
+				title : {
+					media : {word : 'Pleasant'}, //Name of the category presented in the task.
+					css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+					height : 4 //Used to position the "Or" in the combined block.
+				}, 
+				media : [ //Stimuli
+					{word: 'Paradise'},
+					{word: 'Pleasure'},
+					{word: 'Cheer'},
+					{word: 'Wonderful'},
+					{word: 'Splendid'},
+					{word: 'Love'}
+				], 
+				//Can change color and size of the targets here.
+				css : {color:'#31b404','font-size':'2em'}
+						},
 
   base_url : {//Where are your images at?
     image : 'https://baranan.github.io/minno-tasks/images/'
